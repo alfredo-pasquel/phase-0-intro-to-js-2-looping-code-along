03a8d4013acf0a5b62c63d35217319c57d9be639
Alfredo Pasquel <alfredo.pasquel@gmail.com>
2024-07-24 16:32:03 -0400

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const surprise = "surprise"
+
+function writeCards(names, event) {
+
+    const newArray = []
+
+    for (let x = 0; x < names.length; x++) {
+
+    newArray.push(`Thank you, ${names[x]}, for the wonderful ${event} gift!`)
+
+    }
+
+    return newArray;
+
+}
+
+function countDown(start) {
+
+let x = start;
+
+while (x >= 0) {
+  console.log(x--);
+}
+}
+
+
+
+
